4631d10e3f76a3204fdc8562d7c43368d624670e
Arturs Grebnevs <arturs.grebnevs@bertelsmann.de>
2020-08-03 17:21:45 +0300

    Added switch cases

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const buttonContainer = document.querySelector(".button-container");
 const signs = ["%","CE","C","<-","1/x","x^2","#","/","7","8","9","*","4","5","6","-","1","2","3","+","+/-","0",".","="];
+const buttonContainer = document.querySelector(".button-container");
+const result = document.querySelector('.result');
 
-signs.forEach( (sign) => createSignButton(sign));
+signs.forEach((sign) => createSignButton(sign));
 
 function createSignButton(signText) {
     const button = document.createElement("button");
@@ -11,9 +12,25 @@ function createSignButton(signText) {
 }
 
 const buttons = document.querySelectorAll('.button');
-const result = document.querySelector('.result');
 buttons.forEach((button) => button.addEventListener('click',function(e){
     //TODO: add a switch statement for special signs
+    switch(this.innerText){
+        case '%':
+        console.log('Case works');
+        break;
+        case 'CE':
+        console.log('Case works');
+        break;
+        case 'CE':
+        console.log('Case works');
+        break;
+        case 'CE':
+        console.log('Case works');
+        break;
+        case 'CE':
+        console.log('Case works');
+        break;
+    }
     result.innerText += this.innerText;
 }))
 
