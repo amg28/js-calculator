cef8d5cb0736cf598ec9aac2291037292a0ab253
ageek19 <amg2896@gmail.com>
2020-08-06 00:14:46 +0300

    Refactored code

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 //Constants
-const signs = ["%","CE","C","<-","1/x","x^2","#","/","7","8","9","*","4","5","6","-","1","2","3","+","+/-","0",".","="];
+const signs = ["%", "CE", "C", "<-", "1/x", "x^2", "#", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="];
 const buttonContainer = document.querySelector(".button-container");
 
 //Sign button creation
-signs.forEach((sign) => createSignButton(sign));
+signs.forEach((sign) => createButton(sign));
 
-function createSignButton(signText) {
+function createButton(sign) {
     const button = document.createElement("button");
     button.classList.add('button');
-    button.id = signText;
-    button.innerText = signText;
+    button.id = sign;
+    button.innerText = sign;
     buttonContainer.appendChild(button);
 }
 
--- math-operations.js
+++ math-operations.js
@@ -2,54 +2,58 @@
 const screen = document.querySelector('.screen');
 
 // Variables used for mathematical operations
-let firstNumber = 0;
-let secondNumber = 0;
+let numberOne = 0;
+let numberTwo = 0;
 let operation = '';
 
 //Adding event listeners to buttons
 const buttons = document.querySelectorAll('.button');
-buttons.forEach((button) => button.addEventListener('click',function(e){
-    switch(this.id){
+buttons.forEach((button) => button.addEventListener('click', function (e) {
+
+    //Each button's id is defined as math operation sign. Example: '+' button's id is set as '+'.
+    const sign = this.id;
+
+    switch (sign) {
         case '+':
         case '-':
         case '*':
         case '/':
-            firstNumber = parseFloat(screen.innerText);
+            numberOne = parseFloat(screen.innerText);
             clearScreen(screen);
-            operation = this.id;
+            operation = sign;
             break;
-        
+
         case '=':
-            secondNumber = parseFloat(screen.innerText);
+            numberTwo = parseFloat(screen.innerText);
             clearScreen(screen);
-            screen.innerText = calculate(firstNumber, secondNumber, operation);
+            screen.innerText = calculate(numberOne, numberTwo, operation);
             console.log(`Final screen: ${screen.innerText}`);
             break;
 
         default:
             show(screen, this.innerText);
             break;
-    }    
+    }
 }))
 
-function calculate(numberOne, numberTwo, operation){
+function calculate(numberOne, numberTwo, operation) {
     console.info(`1: ${numberOne} 2: ${numberTwo} 3: ${operation}`);
-    switch(operation){
+    switch (operation) {
         case '+':
-            return numberOne+numberTwo;
+            return numberOne + numberTwo;
         case '-':
-            return numberOne-numberTwo;
+            return numberOne - numberTwo;
         case '*':
-            return numberOne*numberTwo;
+            return numberOne * numberTwo;
         case '/':
-            return numberOne/numberTwo;
+            return numberOne / numberTwo;
     }
 }
 
-function show(screen, number){
+function show(screen, number) {
     screen.innerText += number;
 }
 
-function clearScreen(screen){
+function clearScreen(screen) {
     screen.innerText = '';
 }
